e85aa60c3892f75a914326d140d5ef93f16894e1
Rose Linehan <Rose@roses-mbp.lan>
2020-06-10 18:04:33 -0400

    completed ride-hailing, pt1

---
+++ notuber/README.md
@@ -0,0 +1,16 @@
+# LAB - THE RIDE-HAILING SERVICE
+
+Everything is implemented correctly, I believe.  
+
+I did not collaborate with anyone on this lab. 
+
+I spent about 3 hours on it. 
+
+## Performance without optimization: 
+
+| File   | Size (B) | Load Time (ms) |
+|--------|-----|-----------|
+| app.js | 1000 | 4 |
+| style.css | 315 | 4 |
+
+
---
+++ notuber/app.js
@@ -0,0 +1,3 @@
+var data=[{"id":"mXfkjrFw","lat":42.3453,"long":-71.0464},{"id":"nZXB8ZHz","lat":42.3662,"long":-71.0621},{"id":"Tkwu74WC","lat":42.3603,"long":-71.0547},{"id":"5KWpnAJN","lat":42.3472,"long":-71.0802},{"id":"uf5ZrXYw","lat":42.3663,"long":-71.0544},{"id":"VMerzMH8","lat":42.3542,"long":-71.0704}]
+var map;function initMap(){var icon={url:"car.png",scaledSize:new google.maps.Size(15,35)}
+map=new google.maps.Map(document.getElementById("map"),{center:{lat:42.352271,lng:-71.05524200000001},zoom:14});for(i=0;i<data.length;i++){var marker=new google.maps.Marker({position:{lat:data[i].lat,lng:data[i].long},map:map,icon:icon})}}
